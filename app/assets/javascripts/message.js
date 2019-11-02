dbce3a2a22b6edad6ca943350554370f3dd564f0
takeda-shin <kgcfy958@yahoo.co.jp>
2019-11-02 22:50:27 +0900

    add respond_to controller

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,11 @@
 $(function(){
   $('#form').on('submit', function(e) {
     e.preventDefault();
-    var formData = new FormFata(this);
+    var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
       url: url,
-      type: 'POST',
+      type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
