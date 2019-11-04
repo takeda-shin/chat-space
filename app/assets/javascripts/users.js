8d8976ee3ee4b1270e7127fe2671fea48e23c3f1
takeda-shin <kgcfy958@yahoo.co.jp>
2019-11-04 20:23:35 +0900

    ajax when event firing

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,6 +2,17 @@ $(function() {
 
   $('#user-search-field').on('keyup', function() {
     let input = $('#user-search-field').val();
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      dataType: 'json',
+      data: { keyword: input },
+    })
+      .done(function(users) {
+        console.log("成功です");
+      })
+      .fail(function() {
+        console.log("失敗です");
+      })
   })
 })
